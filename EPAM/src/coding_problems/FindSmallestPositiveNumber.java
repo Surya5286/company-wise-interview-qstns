da56268f154d70dd8bae5bac8a10b4d8c249400e
surya <annakalyan286@gmail.com>
2026-03-12 20:53:25 +0530

    Q1 Hard Question

--- EPAM/src/coding_problems/FindSmallestPositiveNumber.java
+++ EPAM/src/coding_problems/FindSmallestPositiveNumber.java
@@ -3,7 +3,6 @@ package coding_problems;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class FindSmallestPositiveNumber {
     public static void main(String[] args) {
@@ -14,53 +13,28 @@ public class FindSmallestPositiveNumber {
         System.out.println("The smallest missing positive number is: " + findSmallestMissingPositiveUsingSorting(arr2));
 
         int[] arr3 = {7, 8, 9, 11, 12};
-        System.out.println("The smallest missing positive number is: " + findSmallestMissingPositiveUsingTreeSet(arr3));
+        System.out.println("The smallest missing positive number is: " + findSmallestMissingPositiveUsingOptimalApproach(arr3));
 
         int[] arr4 = {-3, -2, -1};
-        System.out.println("The smallest missing positive number is: " + findSmallestMissingPositiveUsingTreeSet(arr4));
+        System.out.println("The smallest missing positive number is: " + findSmallestMissingPositiveUsingOptimalApproach(arr4));
 
         int[] arr5 = {-3, -2, -1, 1, 2, 3};
         System.out.println("The smallest missing positive number is: " + findSmallestMissingPositiveUsingSet(arr5));
     }
 
+    // TC - O[NlogN] SC - O[1]
     private static int findSmallestMissingPositiveUsingSorting(int[] arr) {
 
+        if(arr == null || arr.length == 0)
+            return 1;
+
         Arrays.sort(arr);
-        int smallestMissing = 1;
-        for (int i = 0; i < arr.length; i++) {
-
-            if(arr[i] <= 0)
-                continue;
-
-            if (i+1 < arr.length && arr[i] == arr[i + 1])
-                continue; // Skip duplicates
-            else {
-                if (arr[i] == smallestMissing) {
-                    smallestMissing++;
-                } else if (arr[i] > smallestMissing) {
-                    break; // No need to check further
-                }
-            }
-
-        }
-
-        return smallestMissing;
-    }
-
-    private static int findSmallestMissingPositiveUsingTreeSet(int[] arr) {
 
         int smallestMissing = 1;
-        Set<Integer> set = new TreeSet<>();
-
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > 0)
-                set.add(arr[i]);
-        }
-
-        for (Integer num : set) {
-            if (num == smallestMissing) {
+            if (arr[i] == smallestMissing) {
                 smallestMissing++;
-            } else if (num > smallestMissing) {
+            } else if (arr[i] > smallestMissing) {
                 break; // No need to check further
             }
         }
@@ -68,6 +42,7 @@ public class FindSmallestPositiveNumber {
         return smallestMissing;
     }
 
+    // TC - O[n] SC - O[n]
     private static int findSmallestMissingPositiveUsingSet(int[] arr) {
         int smallestMissing = 1;
 
@@ -86,5 +61,51 @@ public class FindSmallestPositiveNumber {
         }
 
         return smallestMissing;
+    }
+
+    // TC - O[n] SC - O[1]
+    private static int findSmallestMissingPositiveUsingOptimalApproach(int[] arr) {
+
+        // Edge Cases
+        if(arr == null || arr.length == 0)
+            return 1;
+
+        /*
+            As in above approach - how we used an external set, now we will update the same concept but within the same array.+
+            Step1:- First iterate through the list and update all negative numbers to n+1
+            Step2:- Iterate through loop,
+                get the absolute value of index then check
+                if value == 0, continue
+                Get the value's index position -> update that value in that position to negative
+            Step3:- Loop through array, if index is positive -> return (index + 1)
+
+
+        */
+
+        int n = arr.length;
+
+        // Step1
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] <= 0 || arr[i] > n)
+                arr[i] = n+1;
+        }
+
+        // Step2
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(arr[i]);
+            if(num == n+1)
+                continue;   // Skip placeholder value
+            int index = num-1;  // Convert to 0 Based index
+            if(arr[index] > 0)
+                arr[index] = -arr[index];   // Mark as present by making negative.
+        }
+
+        // Step3
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > 0)
+                return i+1;
+        }
+
+        return n+1;
     }
 }
